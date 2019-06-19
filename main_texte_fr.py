3a6f268a94e51b083cf22648726236808871dc8c
nath54 <cerisara.nathan@gmail.com>
2019-06-19 09:36:28 +0100

    a

--- main_texte_fr.py
+++ main_texte_fr.py
@@ -1,9 +1,27 @@
 #coding:utf-8
 #launcher texte
 import os,sys
-import requests
-import zipfile
-import shutil
+try:
+    import requests
+except:
+    print("La librairie requests va etre installée sur votre ordinateur")
+    os.system("pip install requests")
+    import requests
+try: import zipfile
+except:
+    print("La librairie zipfile va etre installée sur votre ordinateur")
+    os.system("pip install zipfile")
+    import zipfile
+try: import shutil
+except:
+    print("La librairie shutil va etre installée sur votre ordinateur")
+    os.system("pip install shutil")
+    import shutil
+try: import subprocess
+except:
+    print("La librairie subprocess va etre installée sur votre ordinateur")
+    os.system("pip install subprocess")
+    import subprocess
 from os.path import expanduser
 home = expanduser("~")
 
@@ -12,14 +30,39 @@ direjeux="jeux/"
 if not direlauncher[:-1] in os.listdir(home):
     os.mkdir(home+"/"+direlauncher)
     os.mkdir(home+"/"+direlauncher+direjeux)
-
 direm=home+"/"+direlauncher
+if not direjeux[:-1] in os.listdir(direm):
+    os.mkdir(home+"/"+direlauncher+direjeux)
 direj=home+"/"+direlauncher+direjeux
 
 def inp(txt):
     vp=sys.version_info
     if vp[0]==2: return raw_input(txt)
     else: return input(txt)
+
+
+fp="params.nath"
+lll=[]
+if not fp in os.listdir(direm):
+    for ll in os.listdir("data/"):
+        if ll[-5:]==".nath": lll.append(ll)
+    print("Wich language do you want ? ")
+    for l in lll:
+        print("    "+str(lll.index(l))+"-"+l.split("_") [1])
+    choix=inp(" : ")
+    try:
+        i=int(choix)
+        l=lll[i]
+    except:
+        print("ERROR : the default language is chosen")
+        l=lll[0]
+    f=open(direm+fp,'w')
+    f.write(l)
+    f.close()
+
+f=open(direm+fp,"r").read()
+tl=open("data/"+f,"r",encoding="utf-8").read().split("\n")
+
 
 c="#"
 cc="|"
@@ -31,11 +74,11 @@ def page_jeu_magasin(jeu):
     jdt=False
     nmj=False
     version_i=""
-    if True:
+    try:
         req = requests.get(jeu[4])
         for chunk in req.iter_content(1000):  version_i+=str(chunk,"utf-8")
         version_i=str(version_i.replace("\n",""))
-    else: print("ERREUR : N'arrive pas à accéder à la dernière version du jeu")
+    except: print(tl[0])
     if jeu[0] in os.listdir(direj):
         jdt=True
         version_p=open(direj+jeu[0]+"/version","r").read()
@@ -44,53 +87,65 @@ def page_jeu_magasin(jeu):
     print("")
     print("#################################################################")
     print("")
-    print("Voici la page du jeu "+str(jeu[0]))
+    print(tl[1]+str(jeu[0]))
     print("")
-    print("dernière version : "+version_i)
-    if jdt: print("version téléchargée : "+version_p)
+    print(tl[2]+version_i)
+    if jdt: print(tl[3]+version_p)
     print("")
-    print("description : "+str(jeu[2]))
+    print(tl[4]+str(jeu[2]))
     print("")
-    if not jdt : print(" 1-télécharger le jeu")
+    if not jdt : print(tl[5])
     elif jdt and nmj:
-        print(" 1-Le jeu n'est pas mis à jour, METTRE A JOUR LE JEU")
+        print(tl[6])
     else:
-        print(" 1-Le jeu est déjà installé et mis à jour")
-    print(" q-menu précédent")
+        print(tl[7])
+    print(tl[8])
     choix=inp(" : ")
     if choix!="q":
         if choix=="1" and (not jdt or nmj):
-            print("Téléchargement du jeu :")
+            print(tl[9])
             try:
-                print(" .netoyage des fichiers inutiles...")
+                print(tl[10])
                 if jeu[0] in os.listdir(direj): shutil.rmtree(direj+jeu[0])
                 if jeu[0]+"1" in os.listdir(direj): shutil.rmtree(direj+jeu[0]+"1")
-                print(" .requete de l'url..")
+                print(tl[11])
                 url=jeu[1]
                 req = requests.get(url)
-                print(" .telechargement du fichier zip...")
+                print(tl[12])
                 txxt=b''
+                tt=0
                 for chunk in req.iter_content(1000):
                     txxt+=chunk
+                    tt+=len(chunk)
+                    if len(str(tt))>9: print(str(tt/10**9)+"Go")
+                    if len(str(tt))>6: print(str(tt/10**6)+"Mo")
+                    if len(str(tt))>3: print(str(tt/10**3)+"Ko")
+                    else: print(str(tt)+"o")
                 f=open(direj+jeu[0]+".zip","wb")
                 f.write(txxt)
                 f.close()
-                print(" .extraction du fichier zip...")
+                print(tl[13])
                 zip_ref = zipfile.ZipFile(direj+jeu[0]+".zip","r")
                 zip_ref.extractall(direj+jeu[0]+"/")
                 zip_ref.close()
-                print(" .netoyage des fichiers inutiles...")
+                print(tl[14])
                 os.remove(direj+jeu[0]+".zip")
-                print(" .arrangement des fichiers...")
+                print(tl[15])
                 df1=direj+jeu[0]+"/"+jeu[0]+"-master/"
                 df2=direj+jeu[0]+"1/"
                 df3=direj+jeu[0]+"/"
                 os.rename(df1,df2)
                 os.rmdir(df3)
                 os.rename(df2,df3)
-                print("Jeu téléchargé.")
+                print(tl[16])
+                print(tl[17])
+                for r in jeu[3].split(','):
+                    p=subprocess.Popen('python -c "import '+r+'"')
+                    if p.returncode==0:
+                        os.system("pip install "+r)
+                    else: print(tl[18]+r+tl[19])
             except:
-                print("ERREUR : Il y a eu une erreur lors du téléchargement du jeu.")
+                print(tl[20])
         page_jeu_magasin(jeu)
 
 def magasin():
@@ -105,33 +160,33 @@ def magasin():
         f.write(txxt)
         f.close()
     except:
-        print("Il y a eu une erreur lors du téléchargement de la liste des jeux.")
-        print("Si il y un probleme, un bug, ou si vous voulez des renseignements, contactez moi à : nathpython@gmail.com")
+        print(tl[21])
+        print(tl[22])
     if "data.nath" in os.listdir(direm):
         f=open(direm+"data.nath","r").read().split(cc)
         for ff in f:
             if ff not in [""," ","\b","'b","b''",'b"']:jeux.append( ff.split(c) )
     else:
-        print("ERREUR : Il y a eu une erreur, veuillez réessayer ultérieurement.")
-        print("Si il y un probleme, un bug, ou si vous voulez des renseignements, contactez moi à : nathpython@gmail.com")
+        print(tl[23])
+        print(tl[22])
     print("")
     print("#################################################################")
     print("")
-    print("Jeux disponibles : ")
+    print(tl[24])
     print("")
     for j in jeux: 
         print(" "+str(jeux.index(j)+1)+"-"+str(j[0]))
     print("")
-    print(" q-menu principal")
+    print(tl[25])
     choix=inp(" : ")
     if choix!="q":
-        if True:
+        try:
             choix=int(choix)-1
             if choix>=0 and choix < len(jeux):
                 page_jeu_magasin(jeux[choix])
             else:
-                print("Vous ne pouvez faire cela.")
-        else: print("ERREUR : Vous ne pouvez faire cela.")
+                print(tl[26])
+        except: print(tl[27])
         magasin()
 
 def calc_taille_dossier(dj,taille):
@@ -149,28 +204,27 @@ def page_jeu_b(dj):
     print("")
     print("#################################################################")
     print("")
-    print("Page du jeu : "+nom)
+    print(tl[28]+nom)
     print("")
-    if len(str(taille)) >= 9: print("taille : "+str(taille/10**9)+" Go")
-    elif len(str(taille)) >= 6: print("taille : "+str(taille/10**6)+" Mo")
-    elif len(str(taille)) >= 3: print("taille : "+str(taille/10**3)+" Ko")
-    else: print("taille : "+str(taille)+" o")
+    if len(str(taille)) >= 9: print(tl[29]+str(taille/10**9)+" Go")
+    elif len(str(taille)) >= 6: print(tl[29]+str(taille/10**6)+" Mo")
+    elif len(str(taille)) >= 3: print(tl[29]+str(taille/10**3)+" Ko")
+    else: print(tl[29]+str(taille)+" o")
     print("")
-    print(" 1-jouer au jeu")
-    print(" 2-désinstaller le jeu")
-    print(" 3-voir la page du jeu du magasin")
-    print(" q-quitter")
+    print(tl[30])
+    print(tl[31])
+    print(tl[32])
+    print(tl[33])
     choix=inp(" : ")
     if choix!="q":
         if choix=="1": os.system("cd "+str(dj)+"&& python main.py")
         if choix=="2":
             print("")
-            print("Etes vous vraiment sur de supprimer ce jeu ? (Y/N)")
+            print(tl[34])
             choix2=inp(" : ")
             if choix2=="Y":
-                print("Désinstallation du jeu...")
+                print(tl[35])
                 shutil.rmtree(dj)
-                os.rmdir(dj)
         if choix=="3":
             if "data.nath" in os.listdir(direm):
                 jex=[]
@@ -181,8 +235,8 @@ def page_jeu_b(dj):
                 for j in jex:
                     if j[0]==nom: jj=j
                 if jj!=None: page_jeu_magasin(jj)
-                else: print("ERREUR : Ne peut pas accéder à la page du jeu du magasin")
-            else: print("ERREUR : Ne peut pas accéder à la page du jeu du magasin")
+                else: print(tl[36])
+            else: print(tl[36])
         if choix not in ["2"]: page_jeu_b(dj)
 
 def bibliotheque():
@@ -191,26 +245,27 @@ def bibliotheque():
     print("")
     print("#################################################################")
     print("")
-    print("Vos jeux : ")
+    print(tl[37])
     print("")
     for j in jeux:
         print(" "+str(jeux.index(j)+1)+"-"+j)
     print("")
-    print(" q-revenir au menu")
+    print(tl[38])
     choix=inp(" : ")
     if choix!="q":
-        if True:
+        try:
             choix=int(choix)-1
             if choix>=0 and choix < len(jeux):
                 page_jeu_b(direj+jeux[choix])
             else:
-                print("Vous ne pouvez faire cela.")
-        else: print("Vous ne pouvez faire cela.")
+                print(tl[39])
+        except: print(tl[39])
         bibliotheque()
     
 
 
 def menu():
+    #try:
     try:
         url="https://raw.githubusercontent.com/nath54/launcher_data/master/data.nath"
         req = requests.get(url)
@@ -221,15 +276,15 @@ def menu():
         f.write(txxt)
         f.close()
     except:
-        print("ERREUR : Il y a eu une erreur lors du téléchargement de la liste des jeux.")
-        print("Si il y un probleme, un bug, ou si vous voulez des renseignements, contactez moi à : nathpython@gmail.com")
+        print(tl[21])
+        print(tl[22])
     print("")
     print("#################################################################")
     print("")
-    print("Vous êtes dans le menu principal, que voulez vous faire ?")
-    print(" 1-voir mes jeux installés")
-    print(" 2-voir les jeux disponibles")
-    print(" q-quitter")
+    print(tl[40])
+    print(tl[41])
+    print(tl[42])
+    print(tl[43])
     choix=inp(" : ")
     if choix == "1": bibliotheque()
     elif choix == "2": magasin()
